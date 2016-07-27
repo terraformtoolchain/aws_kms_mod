8d34292a371ce458a4acd2aa9b6f6cb265e1ede9
Andy A <mambetica@hotmail.com>
2016-07-27 21:02:38 +0100

    Temp remove update items

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
-output "arn" {
-  value = "${aws_kms_key.kms_key.arn}"
-}
-output "key_id" {
-  value = "${aws_kms_key.kms_key.key_id}"
-}
+#output "arn" {
+#  value = "${aws_kms_key.kms_key.arn}"
+#}
+#output "key_id" {
+#  value = "${aws_kms_key.kms_key.key_id}"
+#}
